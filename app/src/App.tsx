4175ea69e957e12138497fcd9b1bdc2e85fc377c
carloskauan <code.line.dark@gmail.com>
2023-07-28 11:03:38 -0300

    Inicialização do React-Native

---
+++ app/src/App.tsx
@@ -0,0 +1,11 @@
+import {Text, SafeAreaView } from "react-native";
+
+const App = () =>{
+  return(
+    <SafeAreaView>
+      <Text>Testando</Text>
+    </SafeAreaView>
+  )
+}
+
+export default App;
